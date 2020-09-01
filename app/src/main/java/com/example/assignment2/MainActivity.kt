1020849d4a20ff9ba8889266dcfae145b8920a30
Brook <brook1kenfemichael@gmail.com>
2020-09-01 17:08:00 -0500

    Task A Complete

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var menu : ArrayList<String> = ArrayList<String>(5)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        menu.addAll(arrayOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
+    }
+
+    fun decideClicked(view: View) {
+        val randomNumber = Random.nextInt(0, 5)
+        textView.text = menu[randomNumber]
     }
 }
